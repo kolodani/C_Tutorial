bbb8c03632680ab8e55cfe28e96412637d953e6d
kolodani <danielvinzia@gmail.com>
2025-07-24 11:51:41 -0300

    003 - Syntax

---
+++ 001-Tutorial/003-Syntax/001-ejemplo1.c
@@ -0,0 +1,7 @@
+#include <stdio.h> // incluye funciones de entrada y salida, en este caso printf()
+
+int main() // funcion de ejecucion del programa
+{
+    printf("Hello World!"); // imprime por pantalla lo que le pasamos como parametro
+    return 0; // fin del programa
+}
