99988eba0f983ac215b50046343c2d29c0b16138
kolodani <danielvinzia@gmail.com>
2025-07-28 10:53:25 -0300

    Declare multiple variables

---
+++ 001-Tutorial/006e-Variable_names_identifiers/001-ejemplo.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    // Good variable name
+    int minutesPerHour = 60;
+
+    // OK, but not so easy to understand what m actually is
+    int m = 60;
+
+    return 0;
+}
