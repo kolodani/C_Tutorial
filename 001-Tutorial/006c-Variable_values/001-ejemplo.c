bb8d5bb9f8bb2af65886a00fb0a2f250e9bb920f
kolodani <danielvinzia@gmail.com>
2025-07-28 10:12:03 -0300

    Variable values

---
+++ 001-Tutorial/006c-Variable_values/001-ejemplo.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+    int myNum = 15;  // myNum is 15
+    myNum = 10;      // myNum is 10
+
+    return 0;
+}
---
+++ 001-Tutorial/006c-Variable_values/003-ejemplo.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    // Create a variable and assing the value 15 to it
+    int myNum = 15;
+
+    // Declare a variable without assinging it a value
+    int myOtherNum;
+
+    // Assing the value of myNum to myOtherNum
+    myOtherNum = myNum;
+
+    // myOtherNum now has 15 as a value
+    printf("%d", myOtherNum);
+
+    return 0;
+}
