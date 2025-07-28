44be3e7711998da4749bc5294b15eebbe63b63b5
kolodani <danielvinzia@gmail.com>
2025-07-28 09:18:19 -0300

    Format specifiers

---
+++ 001-Tutorial/006b-Format_specifiers/001-ejemplo.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main()
+{
+    int myNum = 15;
+    printf("%d", myNum); // Outputs 15
+    return 0;
+}
---
+++ 001-Tutorial/006b-Format_specifiers/005-ejemplo.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("My favorite number is: %d\n", 15);
+    printf("My favorite letter is: %c", 'D');
+
+    return 0;
+}
