46fccac4b2eb9894aec564fdf50deb42a7ced9c6
kolodani <danielvinzia@gmail.com>
2025-07-29 08:37:25 -0300

    Variables examples

---
+++ 001-Tutorial/006f-Variables_examples/001-ejemplo.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    // Student data
+    int studentID = 15;
+    int studentAge = 23;
+    float studentFee = 75.25;
+    char studentGrade = 'B';
+
+    // Print variable
+    printf("Student id: %d\n", studentID );
+    printf("Student age: %d\n", studentAge);
+    printf("Student fee: %f\n", studentFee);
+    printf("Student grade: %c", studentGrade);
+
+    return 0;
+}
