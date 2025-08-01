7723b7739f9efb1dfddd89b754d3467525110243
kolodani <danielvinzia@gmail.com>
2025-08-01 10:03:58 -0300

    else if

---
+++ 001-Tutorial/011c-else_if/001-ejemplo.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    /*
+    if (condition1)
+    {
+        // block of code to be executed if condition1 is true
+    }
+    else if (condition2)
+    {
+        // block of code to be executed if condition1 is false and condition2 is true
+    }
+    else
+    {
+        // block of code to be executed if condition1 is false and condition2 is false
+    }
+    */
+
+    return 0;
+}
---
+++ 001-Tutorial/011c-else_if/002-ejemplo.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    int time = 22;
+    if (time < 10)
+    {
+        printf("Good morning.");
+    }
+    else if (time < 20)
+    {
+        printf("Good day.");
+    }
+    else
+    {
+        printf("Good evening.");
+    }
+    // Outputs "Good evening."
+
+    return 0;
+}
