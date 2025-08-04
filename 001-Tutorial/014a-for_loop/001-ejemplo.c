fe67d2e80aae2dcc97a5adee84116bfc4f5a82e4
kolodani <danielvinzia@gmail.com>
2025-08-04 12:47:29 -0300

    for loop

---
+++ 001-Tutorial/014a-for_loop/001-ejemplo.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+/*
+    for (expression 1; expression 2; expression 3)
+    {
+        // code block to be executed
+    }
+*/
+    return 0;
+}
---
+++ 001-Tutorial/014a-for_loop/004-ejemplo.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int sum = 0;
+    int i;
+
+    for (i = 1; i <= 5; i++)
+    {
+        sum = sum + i;
+    }
+
+    printf("Sum is %d\n", sum);
+
+    return 0;
+}
