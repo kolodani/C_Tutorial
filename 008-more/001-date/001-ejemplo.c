3703e91912627d0cae06fce6203501e2b1abf704
kolodani <danielvinzia@gmail.com>
2025-08-31 10:07:10 -0300

    date and time

---
+++ 008-more/001-date/001-ejemplo.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <time.h>
+
+int main()
+{
+    time_t currentTime;
+    time(&currentTime); // Get the current time
+
+    printf("Current time: %s", ctime(&currentTime));
+    return 0;
+}
