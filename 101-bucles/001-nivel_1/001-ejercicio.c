566f20addad4cff25a9080ba8466fa1fdb985621
kolodani <danielvinzia@gmail.com>
2025-09-04 13:14:55 -0300

    Bucles, N1, 001

---
+++ 101-bucles/001-nivel_1/001-ejercicio.c
@@ -0,0 +1,17 @@
+/*
+    BUCLES
+    NIVEL 1
+    EJERCICIO 01
+    Muestre los números de 1 a 100 de forma descendente.
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    for (int i = 0; i < 100; i++)
+    {
+        printf("%d\n", i + 1);
+    }
+    return 0;
+}
