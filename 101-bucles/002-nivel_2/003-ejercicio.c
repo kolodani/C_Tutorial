ed12af0cef8af0c79e1dd990cce35e90df172c3a
kolodani <danielvinzia@gmail.com>
2025-09-05 16:13:22 -0300

    Bucles, N2, 003

---
+++ 101-bucles/002-nivel_2/003-ejercicio.c
@@ -0,0 +1,23 @@
+/*
+    BUCLES
+    NIVEL 2
+    EJERCICIO 03
+    Calcule la suma de los cuadrados de los 100 primeros números enteros.
+*/
+
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int sumatoria = 0, potencia;
+    int num = 1;
+    printf("La suma de los cuadrados de los primeros 100 numeros enteros es:\n");
+    for (int i = 1; i <= 100; i++)
+    {
+        potencia = i * i;
+        sumatoria += potencia;
+    }
+    printf("%d", sumatoria);
+    return 0;
+}
