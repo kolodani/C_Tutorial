4f2b9e627d5a333f510069b3d81ab5ce6c89a8cc
kolodani <danielvinzia@gmail.com>
2025-09-05 11:11:07 -0300

    Bucles, N1, 004

---
+++ 101-bucles/001-nivel_1/004-ejercicio.c
@@ -0,0 +1,34 @@
+/*
+    BUCLES
+    NIVEL 1
+    EJERCICIO 04
+    Pida dos números y sume todos los números comprendidos entre el primero y el segundo (inclusive).
+    Debe controlarse que el segundo número sea mayor que el primero.
+    */
+
+#include <stdio.h>
+
+int main()
+{
+    int primero, segundo, sumatoria = 0;
+    printf("Ingrese dos numeros, te dare la sumatoria de todos los numeros comprendidos entre ellos.\n");
+    printf("Ingresa el primer numero:\n");
+    printf("> ");
+    scanf("%d", &primero);
+    do
+    {
+        printf("Ingresa el segundo numero:\n");
+        printf("> ");
+        scanf("%d", &segundo);
+        if (primero >= segundo)
+        {
+            printf("Ingrese un numero mayor que %d para continuar.\n", primero);
+        }
+    } while (primero >= segundo);
+    for (int i = primero; i <= segundo; i++)
+    {
+        sumatoria += i;
+    }
+    printf("La suma de los numeros desde %d hasta %d es %d", primero, segundo, sumatoria);
+    return 0;
+}
