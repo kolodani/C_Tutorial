dfe07f950dbd50cac2e46b93a0e3fe77308ed4ca
kolodani <danielvinzia@gmail.com>
2025-09-10 13:30:45 -0300

    Arrays, N2, 003

---
+++ 102-arrays/002-nivel_2/003-ejercicio.c
@@ -0,0 +1,47 @@
+/*
+    ARRAYS
+    NIVEL 2
+    EJERCICIO 03
+    Dado un array A de 10 ints cagado aleatoriamente y con valores comprendidos
+    entre 0 y 100, determinar si se tienen más elementos pares que impares o
+    bien misma cantidad.
+    Rehaga el ejercicio utilizando un único bucle for si empleo más de uno.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main()
+{
+    int arregloA[10];
+    int pares = 0, impares = 0;
+
+    srand(time(NULL));
+    for (int i = 0; i < 10; i++)
+    {
+        arregloA[i] = rand() % 101;
+        if (arregloA[i] % 2 == 0)
+        {
+            pares++;
+        }
+        else
+        {
+            impares++;
+        }
+    }
+    if (pares > impares)
+    {
+        printf("El arreglo tiene mas valores pares que impares: %d contra %d.\n", pares, impares);
+    }
+    else if (impares > pares)
+    {
+        printf("El arreglo tiene mas valores impares que pares: %d contra %d.\n", impares, pares);
+    }
+    else
+    {
+        printf("El arreglo tiene igual cantidad de  valores impares que pares: %d y %d.\n", impares, pares);
+    }
+
+    return 0;
+}
