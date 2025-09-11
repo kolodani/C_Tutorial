b0c9a1d6f27b42e80dd6e1e1c65cf0eeeef5c166
kolodani <danielvinzia@gmail.com>
2025-09-11 13:17:55 -0300

    Arrays, N3, 002

---
+++ 102-arrays/003-nivel_3/002-ejercicio.c
@@ -0,0 +1,35 @@
+/*
+    ARRAYS
+    NIVEL 3
+    EJERCICIO 02
+    Permitir la carga automática de una matriz de dimensión NxN y mostrarla por pantalla.
+    */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main()
+{
+    int tamanio;
+    srand(time(NULL));
+    do
+    {
+        printf("Elija un tamaño para una matriz NxN (de 2 a 10): ");
+        scanf("%d", &tamanio);
+    } while (tamanio < 2 || tamanio > 10);
+
+    int matriz[tamanio][tamanio];
+    printf("Tu matriz es:\n");
+    for (int i = 0; i < tamanio; i++)
+    {
+        for (int j = 0; j < tamanio; j++)
+        {
+            matriz[i][j] = rand() % 10;
+            printf("%d\t", matriz[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
