62128e3b0c44fc717770b9d159aacf513ce892de
kolodani <danielvinzia@gmail.com>
2025-09-25 23:35:44 -0300

    Repaso, 004

---
+++ 104-repasos_avanzados/004-ejercicio.c
@@ -0,0 +1,52 @@
+/*
+    PRACTICA 1
+    NIVEL 1
+    EJERCICIO 04:
+    MATRIZ TRANSPUESTA
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int columnas, filas, i, j;
+    printf("Ingresa el numero de Columnas: ");
+    scanf("%d", &columnas);
+
+    printf("Ingresa el numero de Filas: ");
+    scanf("%d", &filas);
+    int matriz[filas][columnas];
+
+    printf("Ingrese ahora los valores de la matriz:\n");
+    for (i = 0; i < filas; i++)
+    {
+        for (j = 0; j < columnas; j++)
+        {
+            printf("Ingresa el valor %d-%d: ", i + 1, j + 1);
+            scanf("%d", &matriz[i][j]);
+        }
+    }
+
+    printf("MATRIZ ORIGINAL:\n");
+    for (int i = 0; i < filas; i++)
+    {
+        for (int j = 0; j < columnas; j++)
+        {
+            printf("%d ", matriz[i][j]);
+        }
+        printf("\n");
+    }
+
+    printf("MATRIZ TRANSPUESTA:\n");
+    for (int i = 0; i < columnas; i++)
+    {
+        for (int j = 0; j < filas; j++)
+        {
+            printf("%d ", matriz[j][i]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
+
+
